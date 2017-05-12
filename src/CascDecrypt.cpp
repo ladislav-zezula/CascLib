d17f58a276b3ec0ffcbc2321f79eb9e0f0e90d33
Jeromnimo <jerome@bausse.fr>
2017-05-12 08:27:52 +0200

    Revert stylistic modification

--- src/CascDecrypt.cpp
+++ src/CascDecrypt.cpp
@@ -17,14 +17,14 @@
 
 typedef struct _CASC_ENCRYPTION_KEY
 {
-  ULONGLONG KeyName;                  // "Name" of the key
-  BYTE Key[0x10];                     // The key itself
+    ULONGLONG KeyName;                  // "Name" of the key
+    BYTE Key[0x10];                     // The key itself
 } CASC_ENCRYPTION_KEY, *PCASC_ENCRYPTION_KEY;
 
 typedef struct _CASC_SALSA20
 {
-  DWORD Key[0x10];
-  DWORD dwRounds;
+    DWORD Key[0x10];
+    DWORD dwRounds;
 
 } CASC_SALSA20, *PCASC_SALSA20;
 
@@ -121,7 +121,7 @@ static CASC_ENCRYPTION_KEY CascKeys[] =
   //  {0x486A2A3A2803BE89ULL, {0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??, 0x??}},   // 95  WOW-23789patch7.2.0_PTR
   { 0, { 0 } }
 };
-
+                                                                                                                           
 static const char * szKeyConstant16 = "expand 16-byte k";
 static const char * szKeyConstant32 = "expand 32-byte k";
 
@@ -130,141 +130,141 @@ static const char * szKeyConstant32 = "expand 32-byte k";
 
 static DWORD Rol32(DWORD dwValue, DWORD dwRolCount)
 {
-  return (dwValue << dwRolCount) | (dwValue >> (32 - dwRolCount));
+    return (dwValue << dwRolCount) | (dwValue >> (32 - dwRolCount));
 }
 
 static LPBYTE FindCascKey(ULONGLONG KeyName)
 {
-  // Search the known keys
-  for (size_t i = 0; CascKeys[i].KeyName != 0; i++)
-  {
-    if (CascKeys[i].KeyName == KeyName)
-      return CascKeys[i].Key;
-  }
+    // Search the known keys
+    for(size_t i = 0; CascKeys[i].KeyName != 0; i++)
+    {
+        if(CascKeys[i].KeyName == KeyName)
+            return CascKeys[i].Key;
+    }
 
-  // Key not found
-  return NULL;
+    // Key not found
+    return NULL;
 }
 
 static void Initialize(PCASC_SALSA20 pState, LPBYTE pbKey, DWORD cbKeyLength, LPBYTE pbVector)
 {
-  const char * szConstants = (cbKeyLength == 32) ? szKeyConstant32 : szKeyConstant16;
-  DWORD KeyIndex = cbKeyLength - 0x10;
+    const char * szConstants = (cbKeyLength == 32) ? szKeyConstant32 : szKeyConstant16;
+    DWORD KeyIndex = cbKeyLength - 0x10;
 
-  memset(pState, 0, sizeof(CASC_SALSA20));
-  pState->Key[0] = *(PDWORD)(szConstants + 0x00);
-  pState->Key[1] = *(PDWORD)(pbKey + 0x00);
-  pState->Key[2] = *(PDWORD)(pbKey + 0x04);
-  pState->Key[3] = *(PDWORD)(pbKey + 0x08);
-  pState->Key[4] = *(PDWORD)(pbKey + 0x0C);
-  pState->Key[5] = *(PDWORD)(szConstants + 0x04);
-  pState->Key[6] = *(PDWORD)(pbVector + 0x00);
-  pState->Key[7] = *(PDWORD)(pbVector + 0x04);
-  pState->Key[8] = 0;
-  pState->Key[9] = 0;
-  pState->Key[10] = *(PDWORD)(szConstants + 0x08);
-  pState->Key[11] = *(PDWORD)(pbKey + KeyIndex + 0x00);
-  pState->Key[12] = *(PDWORD)(pbKey + KeyIndex + 0x04);
-  pState->Key[13] = *(PDWORD)(pbKey + KeyIndex + 0x08);
-  pState->Key[14] = *(PDWORD)(pbKey + KeyIndex + 0x0C);
-  pState->Key[15] = *(PDWORD)(szConstants + 0x0C);
+    memset(pState, 0, sizeof(CASC_SALSA20));
+    pState->Key[0]  = *(PDWORD)(szConstants + 0x00);
+    pState->Key[1]  = *(PDWORD)(pbKey + 0x00);
+    pState->Key[2]  = *(PDWORD)(pbKey + 0x04);
+    pState->Key[3]  = *(PDWORD)(pbKey + 0x08);
+    pState->Key[4]  = *(PDWORD)(pbKey + 0x0C);
+    pState->Key[5]  = *(PDWORD)(szConstants + 0x04);
+    pState->Key[6]  = *(PDWORD)(pbVector + 0x00);
+    pState->Key[7]  = *(PDWORD)(pbVector + 0x04);
+    pState->Key[8]  = 0;
+    pState->Key[9]  = 0;
+    pState->Key[10] = *(PDWORD)(szConstants + 0x08);
+    pState->Key[11] = *(PDWORD)(pbKey + KeyIndex + 0x00);
+    pState->Key[12] = *(PDWORD)(pbKey + KeyIndex + 0x04);
+    pState->Key[13] = *(PDWORD)(pbKey + KeyIndex + 0x08);
+    pState->Key[14] = *(PDWORD)(pbKey + KeyIndex + 0x0C);
+    pState->Key[15] = *(PDWORD)(szConstants + 0x0C);
 
-  pState->dwRounds = 20;
+    pState->dwRounds = 20;
 }
 
 static int Decrypt(PCASC_SALSA20 pState, LPBYTE pbOutBuffer, LPBYTE pbInBuffer, size_t cbInBuffer)
 {
-  LPBYTE pbXorValue;
-  DWORD KeyMirror[0x10];
-  DWORD XorValue[0x10];
-  DWORD BlockSize;
-  DWORD i;
+    LPBYTE pbXorValue;
+    DWORD KeyMirror[0x10];
+    DWORD XorValue[0x10];
+    DWORD BlockSize;
+    DWORD i;
 
-  // Repeat until we have data to read
-  while (cbInBuffer > 0)
-  {
-    // Create the copy of the key
-    memcpy(KeyMirror, pState->Key, sizeof(KeyMirror));
-
-    // Shuffle the key
-    for (i = 0; i < pState->dwRounds; i += 2)
+    // Repeat until we have data to read
+    while(cbInBuffer > 0)
     {
-      KeyMirror[0x04] ^= Rol32((KeyMirror[0x00] + KeyMirror[0x0C]), 0x07);
-      KeyMirror[0x08] ^= Rol32((KeyMirror[0x04] + KeyMirror[0x00]), 0x09);
-      KeyMirror[0x0C] ^= Rol32((KeyMirror[0x08] + KeyMirror[0x04]), 0x0D);
-      KeyMirror[0x00] ^= Rol32((KeyMirror[0x0C] + KeyMirror[0x08]), 0x12);
+        // Create the copy of the key
+        memcpy(KeyMirror, pState->Key, sizeof(KeyMirror));
 
-      KeyMirror[0x09] ^= Rol32((KeyMirror[0x05] + KeyMirror[0x01]), 0x07);
-      KeyMirror[0x0D] ^= Rol32((KeyMirror[0x09] + KeyMirror[0x05]), 0x09);
-      KeyMirror[0x01] ^= Rol32((KeyMirror[0x0D] + KeyMirror[0x09]), 0x0D);
-      KeyMirror[0x05] ^= Rol32((KeyMirror[0x01] + KeyMirror[0x0D]), 0x12);
+        // Shuffle the key
+        for(i = 0; i < pState->dwRounds; i += 2)
+        {
+            KeyMirror[0x04] ^= Rol32((KeyMirror[0x00] + KeyMirror[0x0C]), 0x07);
+            KeyMirror[0x08] ^= Rol32((KeyMirror[0x04] + KeyMirror[0x00]), 0x09);
+            KeyMirror[0x0C] ^= Rol32((KeyMirror[0x08] + KeyMirror[0x04]), 0x0D);
+            KeyMirror[0x00] ^= Rol32((KeyMirror[0x0C] + KeyMirror[0x08]), 0x12);
 
-      KeyMirror[0x0E] ^= Rol32((KeyMirror[0x0A] + KeyMirror[0x06]), 0x07);
-      KeyMirror[0x02] ^= Rol32((KeyMirror[0x0E] + KeyMirror[0x0A]), 0x09);
-      KeyMirror[0x06] ^= Rol32((KeyMirror[0x02] + KeyMirror[0x0E]), 0x0D);
-      KeyMirror[0x0A] ^= Rol32((KeyMirror[0x06] + KeyMirror[0x02]), 0x12);
+            KeyMirror[0x09] ^= Rol32((KeyMirror[0x05] + KeyMirror[0x01]), 0x07);
+            KeyMirror[0x0D] ^= Rol32((KeyMirror[0x09] + KeyMirror[0x05]), 0x09);
+            KeyMirror[0x01] ^= Rol32((KeyMirror[0x0D] + KeyMirror[0x09]), 0x0D);
+            KeyMirror[0x05] ^= Rol32((KeyMirror[0x01] + KeyMirror[0x0D]), 0x12);
 
-      KeyMirror[0x03] ^= Rol32((KeyMirror[0x0F] + KeyMirror[0x0B]), 0x07);
-      KeyMirror[0x07] ^= Rol32((KeyMirror[0x03] + KeyMirror[0x0F]), 0x09);
-      KeyMirror[0x0B] ^= Rol32((KeyMirror[0x07] + KeyMirror[0x03]), 0x0D);
-      KeyMirror[0x0F] ^= Rol32((KeyMirror[0x0B] + KeyMirror[0x07]), 0x12);
+            KeyMirror[0x0E] ^= Rol32((KeyMirror[0x0A] + KeyMirror[0x06]), 0x07);
+            KeyMirror[0x02] ^= Rol32((KeyMirror[0x0E] + KeyMirror[0x0A]), 0x09);
+            KeyMirror[0x06] ^= Rol32((KeyMirror[0x02] + KeyMirror[0x0E]), 0x0D);
+            KeyMirror[0x0A] ^= Rol32((KeyMirror[0x06] + KeyMirror[0x02]), 0x12);
 
-      KeyMirror[0x01] ^= Rol32((KeyMirror[0x00] + KeyMirror[0x03]), 0x07);
-      KeyMirror[0x02] ^= Rol32((KeyMirror[0x01] + KeyMirror[0x00]), 0x09);
-      KeyMirror[0x03] ^= Rol32((KeyMirror[0x02] + KeyMirror[0x01]), 0x0D);
-      KeyMirror[0x00] ^= Rol32((KeyMirror[0x03] + KeyMirror[0x02]), 0x12);
+            KeyMirror[0x03] ^= Rol32((KeyMirror[0x0F] + KeyMirror[0x0B]), 0x07);
+            KeyMirror[0x07] ^= Rol32((KeyMirror[0x03] + KeyMirror[0x0F]), 0x09);
+            KeyMirror[0x0B] ^= Rol32((KeyMirror[0x07] + KeyMirror[0x03]), 0x0D);
+            KeyMirror[0x0F] ^= Rol32((KeyMirror[0x0B] + KeyMirror[0x07]), 0x12);
 
-      KeyMirror[0x06] ^= Rol32((KeyMirror[0x05] + KeyMirror[0x04]), 0x07);
-      KeyMirror[0x07] ^= Rol32((KeyMirror[0x06] + KeyMirror[0x05]), 0x09);
-      KeyMirror[0x04] ^= Rol32((KeyMirror[0x07] + KeyMirror[0x06]), 0x0D);
-      KeyMirror[0x05] ^= Rol32((KeyMirror[0x04] + KeyMirror[0x07]), 0x12);
+            KeyMirror[0x01] ^= Rol32((KeyMirror[0x00] + KeyMirror[0x03]), 0x07);
+            KeyMirror[0x02] ^= Rol32((KeyMirror[0x01] + KeyMirror[0x00]), 0x09);
+            KeyMirror[0x03] ^= Rol32((KeyMirror[0x02] + KeyMirror[0x01]), 0x0D);
+            KeyMirror[0x00] ^= Rol32((KeyMirror[0x03] + KeyMirror[0x02]), 0x12);
 
-      KeyMirror[0x0B] ^= Rol32((KeyMirror[0x0A] + KeyMirror[0x09]), 0x07);
-      KeyMirror[0x08] ^= Rol32((KeyMirror[0x0B] + KeyMirror[0x0A]), 0x09);
-      KeyMirror[0x09] ^= Rol32((KeyMirror[0x08] + KeyMirror[0x0B]), 0x0D);
-      KeyMirror[0x0A] ^= Rol32((KeyMirror[0x09] + KeyMirror[0x08]), 0x12);
+            KeyMirror[0x06] ^= Rol32((KeyMirror[0x05] + KeyMirror[0x04]), 0x07);
+            KeyMirror[0x07] ^= Rol32((KeyMirror[0x06] + KeyMirror[0x05]), 0x09);
+            KeyMirror[0x04] ^= Rol32((KeyMirror[0x07] + KeyMirror[0x06]), 0x0D);
+            KeyMirror[0x05] ^= Rol32((KeyMirror[0x04] + KeyMirror[0x07]), 0x12);
 
-      KeyMirror[0x0C] ^= Rol32((KeyMirror[0x0F] + KeyMirror[0x0E]), 0x07);
-      KeyMirror[0x0D] ^= Rol32((KeyMirror[0x0C] + KeyMirror[0x0F]), 0x09);
-      KeyMirror[0x0E] ^= Rol32((KeyMirror[0x0D] + KeyMirror[0x0C]), 0x0D);
-      KeyMirror[0x0F] ^= Rol32((KeyMirror[0x0E] + KeyMirror[0x0D]), 0x12);
+            KeyMirror[0x0B] ^= Rol32((KeyMirror[0x0A] + KeyMirror[0x09]), 0x07);
+            KeyMirror[0x08] ^= Rol32((KeyMirror[0x0B] + KeyMirror[0x0A]), 0x09);
+            KeyMirror[0x09] ^= Rol32((KeyMirror[0x08] + KeyMirror[0x0B]), 0x0D);
+            KeyMirror[0x0A] ^= Rol32((KeyMirror[0x09] + KeyMirror[0x08]), 0x12);
+
+            KeyMirror[0x0C] ^= Rol32((KeyMirror[0x0F] + KeyMirror[0x0E]), 0x07);
+            KeyMirror[0x0D] ^= Rol32((KeyMirror[0x0C] + KeyMirror[0x0F]), 0x09);
+            KeyMirror[0x0E] ^= Rol32((KeyMirror[0x0D] + KeyMirror[0x0C]), 0x0D);
+            KeyMirror[0x0F] ^= Rol32((KeyMirror[0x0E] + KeyMirror[0x0D]), 0x12);
+        }
+
+        // Set the number of remaining bytes
+        pbXorValue = (LPBYTE)XorValue;
+        BlockSize = (DWORD)CASCLIB_MIN(cbInBuffer, 0x40);
+
+        // Prepare the XOR constants
+        for(i = 0; i < 16; i++)
+        {
+            XorValue[i] = KeyMirror[i] + pState->Key[i];
+        }
+
+        // Decrypt the block
+        for(i = 0; i < BlockSize; i++)
+        {
+            pbOutBuffer[i] = pbInBuffer[i] ^ pbXorValue[i];
+        }
+
+        pState->Key[8] = pState->Key[8] + 1;
+        if(pState->Key[8] == 0)
+            pState->Key[9] = pState->Key[9] + 1;
+
+        // Adjust buffers
+        pbOutBuffer += BlockSize;
+        pbInBuffer += BlockSize;
+        cbInBuffer -= BlockSize;
     }
 
-    // Set the number of remaining bytes
-    pbXorValue = (LPBYTE)XorValue;
-    BlockSize = (DWORD)CASCLIB_MIN(cbInBuffer, 0x40);
-
-    // Prepare the XOR constants
-    for (i = 0; i < 16; i++)
-    {
-      XorValue[i] = KeyMirror[i] + pState->Key[i];
-    }
-
-    // Decrypt the block
-    for (i = 0; i < BlockSize; i++)
-    {
-      pbOutBuffer[i] = pbInBuffer[i] ^ pbXorValue[i];
-    }
-
-    pState->Key[8] = pState->Key[8] + 1;
-    if (pState->Key[8] == 0)
-      pState->Key[9] = pState->Key[9] + 1;
-
-    // Adjust buffers
-    pbOutBuffer += BlockSize;
-    pbInBuffer += BlockSize;
-    cbInBuffer -= BlockSize;
-  }
-
-  return ERROR_SUCCESS;
+    return ERROR_SUCCESS;
 }
 
 static int Decrypt_Salsa20(LPBYTE pbOutBuffer, LPBYTE pbInBuffer, size_t cbInBuffer, LPBYTE pbKey, DWORD cbKeySize, LPBYTE pbVector)
 {
-  CASC_SALSA20 SalsaState;
+    CASC_SALSA20 SalsaState;
 
-  Initialize(&SalsaState, pbKey, cbKeySize, pbVector);
-  return Decrypt(&SalsaState, pbOutBuffer, pbInBuffer, cbInBuffer);
+    Initialize(&SalsaState, pbKey, cbKeySize, pbVector);
+    return Decrypt(&SalsaState, pbOutBuffer, pbInBuffer, cbInBuffer);
 }
 
 //-----------------------------------------------------------------------------
@@ -272,97 +272,97 @@ static int Decrypt_Salsa20(LPBYTE pbOutBuffer, LPBYTE pbInBuffer, size_t cbInBuf
 
 int CascDecrypt(LPBYTE pbOutBuffer, PDWORD pcbOutBuffer, LPBYTE pbInBuffer, DWORD cbInBuffer, DWORD dwFrameIndex)
 {
-  ULONGLONG KeyName = 0;
-  LPBYTE pbBufferEnd = pbInBuffer + cbInBuffer;
-  LPBYTE pbKey;
-  DWORD KeyNameSize;
-  DWORD dwShift = 0;
-  DWORD IVSize;
-  BYTE Vector[0x08];
-  BYTE EncryptionType;
-  int nError;
+    ULONGLONG KeyName = 0;
+    LPBYTE pbBufferEnd = pbInBuffer + cbInBuffer;
+    LPBYTE pbKey;
+    DWORD KeyNameSize;
+    DWORD dwShift = 0;
+    DWORD IVSize;
+    BYTE Vector[0x08];
+    BYTE EncryptionType;
+    int nError;
 
-  // Verify and retrieve the key name size
-  if (pbInBuffer >= pbBufferEnd)
-    return ERROR_FILE_CORRUPT;
-  if (pbInBuffer[0] != 0 && pbInBuffer[0] != 8)
+    // Verify and retrieve the key name size
+    if(pbInBuffer >= pbBufferEnd)
+        return ERROR_FILE_CORRUPT;
+    if(pbInBuffer[0] != 0 && pbInBuffer[0] != 8)
+        return ERROR_NOT_SUPPORTED;
+    KeyNameSize = *pbInBuffer++;
+
+    // Copy the key name
+    if((pbInBuffer + KeyNameSize) >= pbBufferEnd)
+        return ERROR_FILE_CORRUPT;
+    memcpy(&KeyName, pbInBuffer, KeyNameSize);
+    pbInBuffer += KeyNameSize;
+
+    // Verify and retrieve the Vector size
+    if(pbInBuffer >= pbBufferEnd)
+        return ERROR_FILE_CORRUPT;
+    if(pbInBuffer[0] != 4 && pbInBuffer[0] != 8)
+        return ERROR_NOT_SUPPORTED;
+    IVSize = *pbInBuffer++;
+
+    // Copy the initialization vector
+    if((pbInBuffer + IVSize) >= pbBufferEnd)
+        return ERROR_FILE_CORRUPT;
+    memset(Vector, 0, sizeof(Vector));
+    memcpy(Vector, pbInBuffer, IVSize);
+    pbInBuffer += IVSize;
+
+    // Verify and retrieve the encryption type
+    if(pbInBuffer >= pbBufferEnd)
+        return ERROR_FILE_CORRUPT;
+    if(pbInBuffer[0] != 'S' && pbInBuffer[0] != 'A')
+        return ERROR_NOT_SUPPORTED;
+    EncryptionType = *pbInBuffer++;
+
+    // Do we have enough space in the output buffer?
+    if((DWORD)(pbBufferEnd - pbInBuffer) > pcbOutBuffer[0])
+        return ERROR_INSUFFICIENT_BUFFER;
+
+    // Check if we know the key
+    pbKey = FindCascKey(KeyName);
+    if(pbKey == NULL)
+        return ERROR_FILE_ENCRYPTED;
+
+    // Shuffle the Vector with the block index
+    // Note that there's no point to go beyond 32 bits, unless the file has
+    // more than 0xFFFFFFFF frames.
+    for(int i = 0; i < sizeof(dwFrameIndex); i++)
+    {
+        Vector[i] = Vector[i] ^ (BYTE)((dwFrameIndex >> dwShift) & 0xFF);
+        dwShift += 8;
+    }
+
+    // Perform the decryption-specific action
+    switch(EncryptionType)
+    {
+        case 'S':   // Salsa20
+            nError = Decrypt_Salsa20(pbOutBuffer, pbInBuffer, (pbBufferEnd - pbInBuffer), pbKey, 0x10, Vector);
+            if(nError != ERROR_SUCCESS)
+                return nError;
+
+            // Supply the size of the output buffer
+            pcbOutBuffer[0] = (DWORD)(pbBufferEnd - pbInBuffer);
+            return ERROR_SUCCESS;
+
+//      case 'A':   
+//          return ERROR_NOT_SUPPORTED;
+    }
+
+    assert(false);
     return ERROR_NOT_SUPPORTED;
-  KeyNameSize = *pbInBuffer++;
-
-  // Copy the key name
-  if ((pbInBuffer + KeyNameSize) >= pbBufferEnd)
-    return ERROR_FILE_CORRUPT;
-  memcpy(&KeyName, pbInBuffer, KeyNameSize);
-  pbInBuffer += KeyNameSize;
-
-  // Verify and retrieve the Vector size
-  if (pbInBuffer >= pbBufferEnd)
-    return ERROR_FILE_CORRUPT;
-  if (pbInBuffer[0] != 4 && pbInBuffer[0] != 8)
-    return ERROR_NOT_SUPPORTED;
-  IVSize = *pbInBuffer++;
-
-  // Copy the initialization vector
-  if ((pbInBuffer + IVSize) >= pbBufferEnd)
-    return ERROR_FILE_CORRUPT;
-  memset(Vector, 0, sizeof(Vector));
-  memcpy(Vector, pbInBuffer, IVSize);
-  pbInBuffer += IVSize;
-
-  // Verify and retrieve the encryption type
-  if (pbInBuffer >= pbBufferEnd)
-    return ERROR_FILE_CORRUPT;
-  if (pbInBuffer[0] != 'S' && pbInBuffer[0] != 'A')
-    return ERROR_NOT_SUPPORTED;
-  EncryptionType = *pbInBuffer++;
-
-  // Do we have enough space in the output buffer?
-  if ((DWORD)(pbBufferEnd - pbInBuffer) > pcbOutBuffer[0])
-    return ERROR_INSUFFICIENT_BUFFER;
-
-  // Check if we know the key
-  pbKey = FindCascKey(KeyName);
-  if (pbKey == NULL)
-    return ERROR_FILE_ENCRYPTED;
-
-  // Shuffle the Vector with the block index
-  // Note that there's no point to go beyond 32 bits, unless the file has
-  // more than 0xFFFFFFFF frames.
-  for (int i = 0; i < sizeof(dwFrameIndex); i++)
-  {
-    Vector[i] = Vector[i] ^ (BYTE)((dwFrameIndex >> dwShift) & 0xFF);
-    dwShift += 8;
-  }
-
-  // Perform the decryption-specific action
-  switch (EncryptionType)
-  {
-    case 'S':   // Salsa20
-      nError = Decrypt_Salsa20(pbOutBuffer, pbInBuffer, (pbBufferEnd - pbInBuffer), pbKey, 0x10, Vector);
-      if (nError != ERROR_SUCCESS)
-        return nError;
-
-      // Supply the size of the output buffer
-      pcbOutBuffer[0] = (DWORD)(pbBufferEnd - pbInBuffer);
-      return ERROR_SUCCESS;
-
-      //      case 'A':   
-      //          return ERROR_NOT_SUPPORTED;
-  }
-
-  assert(false);
-  return ERROR_NOT_SUPPORTED;
 }
 
 int CascDirectCopy(LPBYTE pbOutBuffer, PDWORD pcbOutBuffer, LPBYTE pbInBuffer, DWORD cbInBuffer)
 {
-  // Check the buffer size
-  if ((cbInBuffer - 1) > pcbOutBuffer[0])
-    return ERROR_INSUFFICIENT_BUFFER;
+    // Check the buffer size
+    if((cbInBuffer - 1) > pcbOutBuffer[0])
+        return ERROR_INSUFFICIENT_BUFFER;
 
-  // Copy the data
-  memcpy(pbOutBuffer, pbInBuffer, cbInBuffer);
-  pcbOutBuffer[0] = cbInBuffer;
-  return ERROR_SUCCESS;
+    // Copy the data
+    memcpy(pbOutBuffer, pbInBuffer, cbInBuffer);
+    pcbOutBuffer[0] = cbInBuffer;
+    return ERROR_SUCCESS;
 }
 
