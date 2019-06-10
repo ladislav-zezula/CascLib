993c64f9e483972c16b3bcb2ef1952a71a3abd5b
Zezula Ladislav <ladislav.zezula@avast.com>
2019-06-10 09:50:52 +0200

    * Added DLL and import LIB to the repository * Added version info to the DLL

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,24 +1,37 @@
 @echo off
 rem Post-build batch for CascLib project
-rem Called as CascLib.bat $(PlatformName) $(ConfigurationName)
+rem Called as PostBuild.bat $(ProjectName) $(PlatformName) $(ConfigurationName)
 rem Example: CascLib.bat x64 Debug
 
-if not exist ..\aaa goto exit
+if "%1" == "CascLib" goto PostBuild_LIB
+if "%1" == "CascLib_dll" goto PostBuild_DLL
 
-copy src\CascPort.h ..\aaa\inc
-copy src\CascLib.h  ..\aaa\inc
+:PostBuild_LIB
+rem Copy the public headers target include directory
+if not exist ..\aaa goto:eof
+copy /Y .\src\CascPort.h ..\aaa\inc
+copy /Y .\src\CascLib.h  ..\aaa\inc
+if "x%2" == "xWin32" goto PostBuild_LIB_Win32
+if "x%2" == "xx64" goto PostBuild_LIB_Win64
+goto:eof
 
-if x%1 == xWin32 goto PlatformWin32
-if x%1 == xx64 goto PlatformWin64
-goto exit
+:PostBuild_LIB_Win32
+rem Copy the compiled LIB to the target library directory
+copy  /Y .\bin\%1\%2\%3\*.lib    ..\aaa\lib32
+goto:eof
 
-:PlatformWin32
-copy .\bin\CascLib\%1\%2\*.lib    ..\aaa\lib32
-goto exit
+:PostBuild_LIB_Win64
+rem Copy the compiled LIB to the target library directory
+copy  /Y .\bin\%1\%2\%3\*.lib    ..\aaa\lib64
+goto:eof
 
-:PlatformWin64
-copy .\bin\CascLib\%1\%2\*.lib    ..\aaa\lib64
-goto exit
+:PostBuild_DLL
+rem On 32-bit Release version, increment the build number
+if not "x%2" == "xWin32" goto PostBuild_DLL_NoRC
+if not "x%3" == "xRelease" goto PostBuild_DLL_NoRC
+PostBuild.exe .\bin\%1\%2\%3\CascLib.dll .\src\DllMain.rc /pdb
+goto:eof
 
-:exit
-
+:PostBuild_DLL_NoRC
+PostBuild.exe .\bin\%1\%2\%3\CascLib.dll /pdb
+goto:eof
