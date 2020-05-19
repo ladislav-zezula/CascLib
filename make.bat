849b12decb089ca00faa173973d5261c2af17830
Ladislav Zezula <ladislav.zezula@avast.com>
2020-05-19 09:41:36 +0200

    Small changes to the build process

--- make.bat
+++ make.bat
@@ -11,20 +11,20 @@ set PROJECT_DIR=%~dp0
 set LIBRARY_NAME=CascLibWDK
 
 :BUILD_LIB_32
-echo Building %LIBRARY_NAME%.lib 32-bit (free) ...
+echo Building %LIBRARY_NAME%.lib (32-bit) ...
 set DDKBUILDENV=
-call %WDKDIR%\bin\setenv.bat %WDKDIR%\ fre wxp
+call %WDKDIR%\bin\setenv.bat %WDKDIR%\ fre w2k
 cd %PROJECT_DIR%
 build.exe -czgw
 echo.
 
 :COPY_LIB_32
-copy /Y .\objfre_wxp_x86\i386\%LIBRARY_NAME%.lib ..\aaa\lib32\%LIBRARY_NAME%.lib >nul
-del buildfre_wxp_x86.log
+copy /Y .\objfre_w2k_x86\i386\%LIBRARY_NAME%.lib ..\aaa\lib32\%LIBRARY_NAME%.lib >nul
+del buildfre_w2k_x86.log
 echo.
 
 :BUILD_LIB_64
-echo Building %LIBRARY_NAME%.lib 64-bit (free) ...
+echo Building %LIBRARY_NAME%.lib (64-bit) ...
 set DDKBUILDENV=
 call %WDKDIR%\bin\setenv.bat %WDKDIR%\ fre x64 WLH
 cd %PROJECT_DIR%
@@ -37,7 +37,8 @@ del buildfre_wlh_amd64.log
 echo.
 
 :COPY_HEADER
-copy /Y .\src\CascLib.h ..\aaa\inc >nul
+copy /Y .\src\CascLib.h  ..\aaa\inc >nul
+copy /Y .\src\CascPort.h ..\aaa\inc >nul
 
 rem Clean temporary files ...
 if exist build.bat del build.bat
