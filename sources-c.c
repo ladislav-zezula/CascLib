a78a94ba2c19597d958da9eb13ab7223568117f3
Ladislav Zezula <ladislav.zezula@avast.com>
2020-12-29 11:19:35 +0100

    Added deflate part of ZLIB

--- sources-c.c
+++ sources-c.c
@@ -1,9 +1,14 @@
-#pragma warning(disable:4005)       // warning C4005: 'DO1' : macro redefinition
-
 #include "src\jenkins\lookup3.c"
+
+#pragma warning(disable:4005)       // warning C4005: 'DO1' : macro redefinition
+#pragma warning(disable:4242)       // deflate.c(1693) : warning C4242: '=' : conversion from 'unsigned int' to 'Bytef', possible loss of data
 #include "src\zlib\adler32.c"
 #include "src\zlib\crc32.c"
+#include "src\zlib\deflate.c"
+#include "src\zlib\trees.c"
+#include "src\zlib\zutil.c"
+
+#undef COPY                         // Conflicting definition
 #include "src\zlib\inffast.c"
 #include "src\zlib\inflate.c"
 #include "src\zlib\inftrees.c"
-#include "src\zlib\zutil.c"
